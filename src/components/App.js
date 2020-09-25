36de915ef48f93fd9664700bf4e281665f631019
Erik Nuber <erik.nuber@yahoo.com>
2020-09-24 19:47:17 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from '../history';
+import Header from './Header';
+
+const App = () => {
+    return(
+        <div className="ui container" style={{marginTop: '10px'}}>
+            <Router history={history}>
+                <Header/>
+            </Router>
+        </div>
+    )
+};
+
+export default App;
